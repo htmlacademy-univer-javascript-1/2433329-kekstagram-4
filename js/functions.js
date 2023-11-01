614b4f0e00b83e73ac6f13a30a5a9d48e23a1c70
Анастасия Белова <nastya_belova_04@mail.ru>
2023-11-01 20:45:46 +0500

    Задание 5.16 Функции возвращаются

--- js/functions.js
+++ js/functions.js
@@ -2,10 +2,6 @@ function checkStringLength (isString, stringLength) {
   return (isString.length) <= stringLength;
 }
 
-checkStringLength('проверяемая строка', 20);
-checkStringLength('проверяемая строка', 18);
-checkStringLength('проверяемая строка', 10);
-
 function checkPalindrome (isString) {
   const upString = isString.toUpperCase();
   const isLenth = upString.length;
@@ -20,7 +16,24 @@ function checkPalindrome (isString) {
   return true;
 }
 
-checkPalindrome('топот');
-checkPalindrome('ДовОд');
-checkPalindrome('Кекс');
+function checkMeeting(startTime, endTime, meetingStart, meetingDuration) {
+  const startMinutes = convertToMinutes(startTime);
+  const endMinutes = convertToMinutes(endTime);
+  const meetingStartMinutes = convertToMinutes(meetingStart);
+  const meetingEndMinutes = meetingStartMinutes + meetingDuration;
+
+  return meetingStartMinutes >= startMinutes && meetingEndMinutes <= endMinutes;
+}
+
+function convertToMinutes(time) {
+  const [hours, minutes] = time.split(':');
+  const parsedHours = parseInt(hours,10);
+  const parsedMinutes = parseInt(minutes,10);
+  const totalMinutes = parsedHours * 60 + parsedMinutes;
+  return totalMinutes;
+}
+
+export {checkMeeting};
+export {checkStringLength};
+export {checkPalindrome};
 
