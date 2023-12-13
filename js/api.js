677d6f598ab5cbfc0f842c786ae2ec5b2ba8d7b8
Анастасия Белова <belovalena331@gmail.com>
2023-12-13 18:31:22 +0500

    Добавление api.js + изменение в form.js

---
+++ js/api.js
@@ -0,0 +1,32 @@
+const SERVER_URL = 'https://30.javascript.pages.academy/kekstagram';
+const ServerRoute = {
+  GET_DATA: '/data',
+  SEND_DATA: '/',
+};
+
+const HttpMethod = {
+  GET: 'GET',
+  POST: 'POST',
+};
+
+const ErrorText = {
+  [HttpMethod.GET]: 'Не удалось загрузить данные. Попробуйте обновить страницу',
+  [HttpMethod.POST]: 'Не удалось отправить форму. Попробуйте ещё раз',
+};
+
+const request = (route, errorText, method = HttpMethod.GET, body = null) =>
+  fetch(`${SERVER_URL}${route}`, { method, body })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Произошла ошибка ${response.status}: ${response.statusText}`);
+      }
+      return response.json();
+    })
+    .catch((err) => ({ error: true, message: errorText || err.message }));
+
+
+const loadPictures = async () => request(ServerRoute.GET_DATA, ErrorText.GET_DATA);
+
+const sendPicture = async (body) => request(ServerRoute.SEND_DATA, ErrorText.SEND_DATA, HttpMethod.POST, body);
+
+export { loadPictures, sendPicture };
